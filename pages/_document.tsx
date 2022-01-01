f6abc969c34474abe912c50a4f18894da40b22a6
maggie-j-liu <maggie.j.liu@gmail.com>
2022-01-01 00:02:45 -0800

    umami

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,12 @@ class MyDocument extends Document {
         <Head>
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
           <link rel="icon" href="/favicon.png" />
+          <script
+            async
+            defer
+            data-website-id="05c056bf-42c9-4662-a291-ae551eec977c"
+            src="https://umami.maggieliu.dev/umami.js"
+          ></script>
         </Head>
         <body>
           <Main />
